a92755f9f99ecf55fa2126396aeb5eb8663e8e9d
Vishwanath Rao <vishymails@gmail.com>
2023-05-11 14:40:13 +0530

    Add files via upload

---
+++ resilience4j-service/src/main/java/com/bvr/DemoController.java
@@ -0,0 +1,59 @@
+package com.bvr;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	public DemoController() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	Logger LOG = LoggerFactory.getLogger(DemoController.class);
+
+	
+	@Autowired
+	private CircuitBreakerFactory circuitBreakerFactory;
+	
+	@Autowired 
+	private HttpBinService httpBin;
+
+	public DemoController(CircuitBreakerFactory circuitBreakerFactory, HttpBinService httpBin) {
+		super();
+		this.circuitBreakerFactory = circuitBreakerFactory;
+		this.httpBin = httpBin;
+	}
+	
+	
+	
+	@GetMapping("/get")
+	public Map get() {
+		return httpBin.get();
+	}
+	
+	
+	@GetMapping("/delay/{seconds}")
+	public Map delay(@PathVariable int seconds) {
+		return circuitBreakerFactory.create("delay").run(httpBin.delaySupplier(seconds), t -> {
+			LOG.warn("delay service call failed error", t);
+			Map<String, String> fallback = new HashMap<>();
+			
+			fallback.put("Message_from_fallback_functionality_Hello ", "Oracle");
+			
+			return fallback;
+		});
+	}
+
+	
+	
+	
+}
