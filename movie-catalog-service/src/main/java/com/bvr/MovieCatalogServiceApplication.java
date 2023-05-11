210e84f1e2e177cd9d1e2d6fc4aebe1534437f47
Vishwanath Rao <vishymails@gmail.com>
2023-05-11 09:57:06 +0530

    Add files via upload

--- movie-catalog-service/src/main/java/com/bvr/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/bvr/MovieCatalogServiceApplication.java
@@ -2,18 +2,22 @@ package com.bvr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class MovieInfoServiceApplication {
+@EnableCircuitBreaker
+public class MovieCatalogServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MovieInfoServiceApplication.class, args);
+		SpringApplication.run(MovieCatalogServiceApplication.class, args);
 	}
 
+	@LoadBalanced
 	@Bean
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
--- ratings-data-service/src/main/java/com/bvr/RatingsDataServiceApplication.java
+++ ratings-data-service/src/main/java/com/bvr/RatingsDataServiceApplication.java
@@ -3,19 +3,13 @@ package com.bvr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class MovieInfoServiceApplication {
+public class RatingsDataServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MovieInfoServiceApplication.class, args);
+		SpringApplication.run(RatingsDataServiceApplication.class, args);
 	}
 
-	@Bean
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
-	}
 }
