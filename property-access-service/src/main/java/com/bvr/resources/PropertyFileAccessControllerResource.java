b5fd6829e67f3c4c42ee08583566a5a1530b3752
Vishwanath Rao <vishymails@gmail.com>
2023-05-12 09:54:00 +0530

    Add files via upload

--- property-access-service/src/main/java/com/bvr/resources/PropertyFileAccessControllerResource.java
+++ property-access-service/src/main/java/com/bvr/resources/PropertyFileAccessControllerResource.java
@@ -1,9 +1,13 @@
 package com.bvr.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.bvr.beans.PropertyAccessBean;
 import com.bvr.beans.PropertyAccessValue;
@@ -21,7 +25,23 @@ public class PropertyFileAccessControllerResource {
 	
 	@GetMapping("accessPropertyFile")
 	public PropertyAccessValue accessPropertyFile() {
+		refreshActuator();
 		return new PropertyAccessValue(propertyAccessBean.getName(), propertyAccessBean.getDescription());
 		
+	}
+	
+	
+	public void refreshActuator() {
+		RestTemplate restTemplate = new RestTemplate();
+		
+		final String baseurl = "http://localhost:8100/actuator/refresh";
+		
+		HttpHeaders headers = new HttpHeaders();
+		
+		headers.set("Content-Type", "application/json");
+		
+		HttpEntity entity = new HttpEntity(headers);
+		
+		ResponseEntity<String> results = restTemplate.postForEntity(baseurl, entity, String.class);
 	}
 }
